6ed6bd041bf5942735c428711438af96f43f0264
xFus10n <razumilovrenat@inbox.lv>
2020-11-16 18:13:39 +0200

    test

--- src/test/java/TestHistogramPlotter.java
+++ src/test/java/TestHistogramPlotter.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class TestHistogramPlotter {
     @Test
     public void name() {
+        //test
         HistogramPlotter plotter = new HistogramPlotter();
         List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         plotter.setProperty("series.count", "9").setData(integers);
