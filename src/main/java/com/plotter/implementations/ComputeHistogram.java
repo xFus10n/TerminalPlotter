52b9bb1119a896a269eb668cbe5d9d501cdcceaa
xFus10n <razumilovrenat@inbox.lv>
2020-11-16 23:27:59 +0200

    basic render, test setup

---
+++ src/main/java/com/plotter/implementations/ComputeHistogram.java
@@ -0,0 +1,39 @@
+package com.plotter.implementations;
+
+import java.util.List;
+
+public class ComputeHistogram {
+    private final List<Integer> data;
+    private final int[][] output;
+
+    public ComputeHistogram(List<Integer> data) {
+        this.data = data;
+        // int[rows][cols]
+        this.output = new int[getMaxValue()][this.data.size()];
+    }
+
+    public int getMaxValue(){
+        int max = 0;
+        for (Integer val : data) {
+            if (val > max) max = val;
+        }
+        return max;
+    }
+
+    public int[][] render(){
+        int count = 0;
+        for (int i = 0; i < getMaxValue(); i++) { //rows
+            int tmpMaxValue = getMaxValue() - count;
+            for (int j = 0; j < this.data.size(); j++) { //cols
+                Integer element = this.data.get(j);
+                if (element == tmpMaxValue) {
+                    output[i][j]=1;
+                }else{
+                    output[i][j]=0;
+                }
+            }
+            count++;
+        }
+        return this.output;
+    }
+}
--- src/test/java/TestHistogramPlotter.java
+++ src/test/java/TestHistogramPlotter.java
@@ -1,6 +1,8 @@
 import com.plotter.*;
 import com.plotter.domain.PlotterTypes;
+import com.plotter.implementations.ComputeHistogram;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,5 +17,25 @@ public class TestHistogramPlotter {
                 .setProperty("series.start", "1")
                 .setData(integers);
         plotter.draw();
+    }
+
+    @Test
+    public void compute() {
+        List<Integer> integers = Arrays.asList(1, 2, 4, 8, 15, 12, 10, 7, 5, 2);
+        ComputeHistogram histo = new ComputeHistogram(integers);
+        int maxValue = histo.getMaxValue();
+        assertEquals(15, maxValue);
+
+        int[][] render = histo.render();
+        for (int[] counts : render) {
+            for (int count : counts) {
+                String pix = "*";
+                String none = " ";
+                if (count==0){
+                    System.out.print(none);
+                }else System.out.print(pix);
+            }
+            System.out.println();
+        }
     }
 }
